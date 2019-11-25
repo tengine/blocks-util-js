3921a09764d023324fc6eddc9f9e9b937170342b
nakanori <n-nakamura@groovenauts.jp>
2019-11-25 19:09:41 +0900

    change format umd

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,17 @@ import babel from 'rollup-plugin-babel';
 import scss from 'rollup-plugin-scss'
 import filesize from 'rollup-plugin-filesize';
 
+import pkg from './package.json';
+
 export default {
   input: 'src/index.js',
   output: {
-    file: 'lib/bundle.js',
-    format: 'cjs'
+    file: pkg.main,
+    format: 'es'
   },
   external: [
     'react', 
+    'react-dom',
   ],
   plugins: [
     resolve({ extensions: ['.jsx', '.js'] }),
