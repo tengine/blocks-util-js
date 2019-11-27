2cad59cfb1b2dba56a70a710fa16d44470a10766
nakanori <n-nakamura@groovenauts.jp>
2019-11-27 11:04:00 +0900

    提供モジュールのバンドル化とcssの分離
    -
    babelでトランスパイルのみだったのをrollupで1モジュールにバンドルする(cjs,
    esm)
    - 提供モジュールのディレクトリ変更
    - scssのディレクトリ変更
    - スタイルをコンポーネントと分離して提供するようにした
    - cssのminify

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: "es" }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    babel({
+      exclude: "node_modules/**",
+    }),
+    resolve({ extensions: [".js", ".jsx"] }),
+    commonjs(),
+  ]
+};
