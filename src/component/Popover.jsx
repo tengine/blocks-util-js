dbd7ac823231e913b151c7938f6ac3fad01460be
nakanori <n-nakamura@groovenauts.jp>
2019-12-18 11:18:27 +0900

    Can't resolve '../../style/triangle.scss'の対処で、名前変更しscssをパッケージに含めるようにした

--- src/component/Popover.jsx
+++ src/component/Popover.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MuiPopover from 'material-ui/Popover';
 import classnames from 'classnames';
 import _ from 'lodash';
-import "../../style/triangle.scss";
+import "../style/index.scss";
 
 const styles = {
   root: {
