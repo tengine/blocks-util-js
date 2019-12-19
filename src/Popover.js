abe205dcb32382585bf0b1a49dd044568ecb3007
nakanori <n-nakamura@groovenauts.jp>
2019-12-19 10:47:46 +0900

    props.arrow=trueでなくてもコンポーネントがサポートしているためstyleとclassNameを渡せるように修正

--- src/Popover.js
+++ src/Popover.js
@@ -14,8 +14,8 @@ const styles = {
 export const Popover = ({ arrow, style, className, ...props }) => {
   return (
     <MuiPopover
-      style={ arrow ? _.defaults(style, styles.root) : style }
-      className={ arrow ? classnames(className, 'triangle', `triangle-${arrow}`) : className }
+      style={ _.defaults(style, styles.root) }
+      className={ classnames(className, 'triangle', `triangle-${arrow}`) }
       { ...props }
       />
   )
